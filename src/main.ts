cecca5d8e1105ee7f7cc7ff6b99f24dbca1f8780
Ben Little <ben.little@benlittle.dev>
2021-08-14 15:33:38 -0400

    refactor and fix async

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,45 @@
 import * as core from "@actions/core";
-import * as tc from "@actions/tool-cache";
-import * as cache from "@actions/cache";
 import * as exec from '@actions/exec';
+import * as tc from "@actions/tool-cache";
 import promises from "fs";
 import { promisify } from "util";
 
 const chmod = promisify(promises.chmod);
+const exists = promisify(promises.exists);
 
 const packages = core.getInput("packages").split(/\s/);
-const version = core.getInput("version");
+const alpine_version = core.getInput("alpine_version");
 const mount = core.getInput("mount");
 const arch = core.getInput("arch");
 const chroot_dir = core.getInput("chroot_dir");
 
+const chroot_exec = async (user: string = "user", cmd: string[] = []): Promise<number> => {
+	if (user !== "root") {
+		cmd = ["-u", user, ...cmd];
+	}
+	return await exec.exec(`${chroot_dir}/enter-chroot`, cmd);
+};
+
 const install = async () => {
-	const installer = await tc.downloadTool(`https://raw.githubusercontent.com/alpinelinux/alpine-chroot-install/v${version}/alpine-chroot-install`);
+	if (await exists(chroot_dir)) {
+		console.log("chroot_dir already exists; not doing anything!")
+		return
+	}
+	const installer = await tc.downloadTool(`https://raw.githubusercontent.com/alpinelinux/alpine-chroot-install/v${alpine_version}/alpine-chroot-install`);
 	await chmod(installer, 0o755);
-	exec.exec("mkdir", ["-p", mount]);
-	exec.exec("sudo", [
+	await exec.exec("mkdir", ["-p", mount]);
+	await exec.exec("sudo", [
 		installer,
 		"-a", arch,
 		"-d", chroot_dir,
 		"-i", mount,
 	]);
-	exec.exec(`${chroot_dir}/enter-chroot`, ["apk", "add", ...packages]);
-	exec.exec(`${chroot_dir}/enter-chroot`, ["adduser", "-D", "user"]);
-};
-
-const run = async () => {
-	const chroot = await cache.restoreCache([`${chroot_dir}`], `alpine-chroot: ${packages.join(" ")}`);
-	if (!chroot) {
-		await install()
-	}
+	await chroot_exec("root", ["apk", "add", ...packages]);
+	await chroot_exec("root", ["adduser", "-D", "user"]);
 };
 
 try {
-	run();
+	install()
 } catch (error) {
-	core.setFailed(error.message);
+	core.setFailed(error)
 }
